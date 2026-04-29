122b423c6589158236bf854eb4c423db81efb087
Madhur Prakash <madhurprakash2005@gmail.com>
2026-04-29 14:55:52 +0530

    Remove ecosystem configuration and axios interceptor for localtunnel

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,8 +4,3 @@ export const axiosInstance = axios.create({
   baseURL: "/api",
   withCredentials: true,
 });
-
-axiosInstance.interceptors.request.use((config) => {
-  config.headers["bypass-tunnel-reminder"] = "true"; // required to bypass the localtunnel reminder page
-  return config;
-});
